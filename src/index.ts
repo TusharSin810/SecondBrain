2dc711be67226f0a52a69cf97f2352a33daf7e4c
TusharSin810 <tsinghal666@gmail.com>
2024-12-19 01:55:29 +0530

    Schema Setup for mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,63 @@
 import express from "express"
 import jwt from "jsonwebtoken"
+import { UserModel } from "./db";
 
 const app = express();
 const port = 3000;
+const JWT_PASSWORD = "Tushar810";
 
-app.post("/api/v1/signup", (req, res) => {
 
+app.post("/api/v1/signup", async (req, res) => {
+    //ZOD Validation
+    const username = req.body.username;
+    const password = req.body.password;
+    try{
+        await UserModel.create({
+            username: username,
+            password: password
+        })
+
+        res.json({
+            message: "User Singed Up"
+        })
+    }catch(e){
+        res.status(411).json({
+            message: "User Already Exist"
+        })
+    }
 })
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userexist = await UserModel.findOne({
+        username: username,
+        password: password
+    })  
+
+    if(userexist){
+        const token = jwt.sign({
+            id: userexist._id
+        }, JWT_PASSWORD)
+        res.json({
+            token
+        }) 
+    }else{
+        res.status(403).json({
+            message: "Iccorect Credentials"
+        })
+    }
+})
+
+app.post("/api/v1/content", (req, res) => {
     
 })
+
+app.get("/api/v1/content", (req, res) => {
+    
+})
+
 
 app.listen(port , () => {
     console.log(`Listening on Port : ${port}`);
