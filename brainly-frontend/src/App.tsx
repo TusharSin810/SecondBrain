10864039a50e8cf2e43b964526e7b38267c2967f
TusharSin810 <tsinghal666@gmail.com>
2024-12-22 19:46:20 +0530

    Created Button Component

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { Button } from './components/Button'
+import { PlusIcon } from './icons/PlusIcon'
 
 function App() {
+  
+  function hi(){
+    console.log("Hello There")
+  }
+
   return (
     <>
-      <div className='text-2xl text-purple-700'>
+      <div className='text-purple-700'>
           Brainly - Second Brain App 
+        <Button variant='primary' text='Add Content' size='sm' startIcon={<PlusIcon size='md' />} onClick={hi}/>
+        <Button variant='secondary' text='Share Link' size='md' onClick={hi}/>
       </div>
     </>
   )
---
+++ brainly-frontend/src/components/Button.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from "react";
+
+interface ButtonProps {
+    variant: "primary" | "secondary";
+    size: "sm" | "md" | "lg";
+    text: string;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;   
+    onClick: () => void;
+}
+
+const defaultStyles = "rounded-md flex"
+
+const sizeStyles = {
+    "sm": "py-1 px-2",
+    "md": "py-2 px-3",
+    "lg": "py-3 py-4",
+} 
+
+const variantstyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-300 text-purple-500", 
+}
+
+export const Button = (props: ButtonProps) => {
+    return <button className={`${variantstyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null} {props.text} {props.endIcon}</button>
+
+}
+
