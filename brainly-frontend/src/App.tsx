a0bc19d08d5bdcad7de963ea454486e1ecd22364
TusharSin810 <tsinghal666@gmail.com>
2024-12-26 23:40:24 +0530

    Brainly Text

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <div className='text-purple-700'>
-          Brainly - Second Brain App 
+          Brainly - Second Brain App For Storing UseFull Links 
         <Button variant='primary' text='Add Content' size='sm' startIcon={<PlusIcon size='md' />} onClick={hi}/>
         <Button variant='secondary' text='Share Link' size='md' onClick={hi} endIcon={<ShareIcon size='md'/>}/>
       </div>
