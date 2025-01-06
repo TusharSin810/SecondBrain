12425da7e509132d55d4302a8ea7df97f875eb03
TusharSin810 <tsinghal666@gmail.com>
2025-01-06 22:48:25 +0530

    Updated

--- brainly-frontend/src/App.tsx
+++ brainly-frontend/src/App.tsx
@@ -23,15 +23,6 @@ function App() {
         <div className='bg-neutral-500 h-52 w-56 m-8'>
             Image
         </div>
-        <div className='bg-neutral-500 h-52 w-56 m-8'>
-            Image
-        </div>
-            <div className='bg-neutral-500 h-52 w-56 m-8'>
-            Image
-        </div>
-        <div className='bg-neutral-500 h-52 w-56 m-8'>
-            Image
-        </div>
     </div>
     </div>
   )
